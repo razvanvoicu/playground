43f9548c6ac5af6ec7df444edd35952ee0448acf
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-06-25 22:12:57 +0800

    Hacker Rank - Euler challenge - Largest Prime Factor - Haskell

--- hackerrank/euler/LargestPalindromeProduct/haskell/LargestPalindromeProduct.hs
+++ hackerrank/euler/LargestPalindromeProduct/haskell/LargestPalindromeProduct.hs
@@ -1,48 +1,30 @@
 import Control.Monad
-import Data.Functor
 import Data.List
 
 main = do
-  count <- read <$> getLine :: IO Int
-  forM_ [1..count] $ \_ -> do
-    n <- read <$> getLine :: IO Int
-    let result = head $ largestPalindrome n
-    putStrLn $ show $ result
+  testCount <- readLn :: IO Int
+  inputs <- replicateM testCount readLn :: IO [Int]
+  forM_ inputs $ print . head . largestPal
 
-largestPalindrome :: Int -> [Int]
-largestPalindrome i = dropWhile (not . two3digitFactors) l
-  where k = i `div` 1000
-        m = [k,k-1..100]
-        p = map (\x -> (read $ (show x) ++ reverse (show x) :: Int)) m
-        l = dropWhile (> i) p
+makePal :: Int -> Int
+makePal = (read .) $ join $ (. reverse . show) . (++) . show 
+  
+largestPal :: Int -> [Int]
+largestPal upperBound = dropWhile (not . two3digitFactors) candidates
+  where first3 = upperBound `div` 1000
+        palPrefixes = [first3,first3-1..100]
+        candidates = dropWhile (> upperBound) $ map makePal palPrefixes
 
-factorProd k = map (\f -> dropWhile (\x -> x < 100 || x >= 1000 || k `div` x < 100 || k `div` x >= 1000) $ map product $ tails f) $ factorPerm k
+not3digits = join $ (. (1000 <=)) . (||) . (100 >)  
 
-two3digitFactors k = (filter (/=[]) $ factorProd k) /= []
+factorProd k = map (\f -> dropWhile isPalCandidate $ map product $ tails f) $ factorPerm k
+  where isPalCandidate = join $ (. not3digits . div k) . (||) . not3digits 
+
+two3digitFactors = notNull . filter notNull . factorProd
+  where notNull :: Eq a => [a] -> Bool
+        notNull = ([] /=)
 
 factorPerm k = permutations $ primeFactors k
-
-primes :: Integral int => [int]
-primes = wheelSieve 6
-
-{-# SPECIALISE primes :: [Int]     #-}
-{-# SPECIALISE primes :: [Integer] #-}
-
-wheelSieve :: Integral int
-           => Int   
-           -> [int] 
-wheelSieve k = reverse ps ++ map head (sieve p (cycle ns))
- where (p:ps,ns) = wheel k
-
-{-# SPECIALISE wheelSieve :: Int -> [Int]     #-}
-{-# SPECIALISE wheelSieve :: Int -> [Integer] #-}
-
-isPrime :: Integral int => int -> Bool
-isPrime n | n > 1     = primeFactors n == [n]
-          | otherwise = False
-
-{-# SPECIALISE isPrime :: Int     -> Bool #-}
-{-# SPECIALISE isPrime :: Integer -> Bool #-}
 
 primeFactors :: Integral int => int -> [int]
 primeFactors n = factors n (wheelSieve 6)
@@ -53,22 +35,20 @@ primeFactors n = factors n (wheelSieve 6)
                    | otherwise = factors m ps
    where (q,r) = quotRem m p
 
-{-# SPECIALISE primeFactors :: Int     -> [Int]     #-}
-{-# SPECIALISE primeFactors :: Integer -> [Integer] #-}
+wheelSieve :: Integral int
+           => Int    -- ^ number of primes canceled by the wheel
+           -> [int]  -- ^ infinite list of primes
+wheelSieve k = reverse ps ++ map head (sieve p (cycle ns))
+ where (p:ps,ns) = wheel k
+
 sieve :: (Ord int, Num int) => int -> [int] -> [[int]]
 sieve p ns@(m:ms) = spin p ns : sieveComps (p+m) ms (composites p ns)
-
-{-# SPECIALISE sieve :: Int     -> [Int]     -> [[Int]]     #-}
-{-# SPECIALISE sieve :: Integer -> [Integer] -> [[Integer]] #-}
 
 type Composites int = (Queue int,[[int]])
 
 composites :: (Ord int, Num int) => int -> [int] -> Composites int
 composites p ns = (Empty, map comps (spin p ns : sieve p ns))
  where comps xs@(x:_) = map (x*) xs
-
-{-# SPECIALISE composites :: Int     -> [Int]     -> Composites Int     #-}
-{-# SPECIALISE composites :: Integer -> [Integer] -> Composites Integer #-}
 
 splitComposites :: Ord int => Composites int -> (int,Composites int)
 splitComposites (Empty, xs:xss) = splitComposites (Fork xs [], xss)
@@ -77,18 +57,10 @@ splitComposites (queue, xss@((x:xs):yss))
   | otherwise = (z, discard z (enqueue zs queue', xss))
  where (z:zs,queue') = dequeue queue
 
-{-# SPECIALISE splitComposites :: Composites Int -> (Int,Composites Int) #-}
-{-# SPECIALISE
-    splitComposites :: Composites Integer -> (Integer,Composites Integer) #-}
-
 discard :: Ord int => int -> Composites int -> Composites int
 discard n ns | n == m    = discard n ms
              | otherwise = ns
  where (m,ms) = splitComposites ns
-
-{-# SPECIALISE discard :: Int -> Composites Int -> Composites Int #-}
-{-# SPECIALISE
-    discard :: Integer -> Composites Integer -> Composites Integer #-}
 
 sieveComps :: (Ord int, Num int) => int -> [int] -> Composites int -> [[int]]
 sieveComps cand ns@(m:ms) xs
@@ -97,31 +69,18 @@ sieveComps cand ns@(m:ms) xs
   | otherwise    = sieveComps cand ns ys
  where (comp,ys) = splitComposites xs
 
-{-# SPECIALISE sieveComps :: Int -> [Int] -> Composites Int -> [[Int]] #-}
-{-# SPECIALISE
-    sieveComps :: Integer -> [Integer] -> Composites Integer -> [[Integer]] #-}
-
 spin :: Num int => int -> [int] -> [int]
 spin x (y:ys) = x : spin (x+y) ys
-
-{-# SPECIALISE spin :: Int     -> [Int]     -> [Int]     #-}
-{-# SPECIALISE spin :: Integer -> [Integer] -> [Integer] #-}
 
 type Wheel int = ([int],[int])
 
 wheel :: Integral int => Int -> Wheel int
 wheel n = iterate next ([2],[1]) !! n
 
-{-# SPECIALISE wheel :: Int -> Wheel Int     #-}
-{-# SPECIALISE wheel :: Int -> Wheel Integer #-}
-
 next :: Integral int => Wheel int -> Wheel int
 next (ps@(p:_),xs) = (py:ps,cancel (product ps) p py ys)
  where (y:ys) = cycle xs
        py = p + y
-
-{-# SPECIALISE next :: Wheel Int     -> Wheel Int     #-}
-{-# SPECIALISE next :: Wheel Integer -> Wheel Integer #-}
 
 cancel :: Integral int => int -> int -> int -> [int] -> [int]
 cancel 0 _ _ _ = []
@@ -130,17 +89,10 @@ cancel m p n (x:ys@(y:zs))
   | otherwise      = cancel m p n (x+y:zs)
  where nx = n + x
 
-{-# SPECIALISE cancel :: Int -> Int -> Int -> [Int] -> [Int] #-}
-{-# SPECIALISE
-    cancel :: Integer -> Integer -> Integer -> [Integer] -> [Integer] #-}
-
 data Queue int = Empty | Fork [int] [Queue int]
 
 enqueue :: Ord int => [int] -> Queue int -> Queue int
 enqueue ns = merge (Fork ns [])
-
-{-# SPECIALISE enqueue :: [Int]     -> Queue Int     -> Queue Int     #-}
-{-# SPECIALISE enqueue :: [Integer] -> Queue Integer -> Queue Integer #-}
 
 merge :: Ord int => Queue int -> Queue int -> Queue int
 merge Empty y                        = y
@@ -150,19 +102,10 @@ merge x     y     | prio x <= prio y = join x y
  where prio (Fork (n:_) _) = n
        join (Fork ns qs) q = Fork ns (q:qs)
 
-{-# SPECIALISE merge :: Queue Int     -> Queue Int     -> Queue Int     #-}
-{-# SPECIALISE merge :: Queue Integer -> Queue Integer -> Queue Integer #-}
-
 dequeue :: Ord int => Queue int -> ([int], Queue int)
 dequeue (Fork ns qs) = (ns,mergeAll qs)
-
-{-# SPECIALISE dequeue :: Queue Int     -> ([Int],     Queue Int)     #-}
-{-# SPECIALISE dequeue :: Queue Integer -> ([Integer], Queue Integer) #-}
 
 mergeAll :: Ord int => [Queue int] -> Queue int
 mergeAll []       = Empty
 mergeAll [x]      = x
 mergeAll (x:y:qs) = merge (merge x y) (mergeAll qs)
-
-{-# SPECIALISE mergeAll :: [Queue Int]     -> Queue Int     #-}
-{-# SPECIALISE mergeAll :: [Queue Integer] -> Queue Integer #-}
