f981cf7878e28deeaa66cf8fb6f0689d502d1ebf
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-07-28 09:59:57 +0800

    Rosalind Scala "Substring" solution.

---
+++ rosalind/scala/src/SUBS.scala
@@ -0,0 +1,11 @@
+import scala.io.Source
+
+object SUBS {
+  def main(args: Array[String]): Unit = {
+    val lines = Source.stdin.getLines.toArray
+    val text = lines(0)
+    val pat = lines(1)
+    val pos = (0 to text.length-1).filter(i => text.indexOf(pat,i) == i).map(_ + 1)
+    println(pos.mkString(" "))
+  }
+}
