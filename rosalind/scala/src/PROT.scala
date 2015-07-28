f7806c06fb4547b79b1e681961448ab9d5762788
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-07-28 10:00:30 +0800

    Rosalind Scala "Protein Translation" solution.

---
+++ rosalind/scala/src/PROT.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+
+object PROT {
+  def main(args:Array[String]): Unit = {
+    val fname = "res/rna_codon.txt"
+    val translator = Source.fromFile(fname).getLines.toArray.map(p => { val a = p.split("[ ]+"); (a(0),a(1))}).toMap
+    val rna = Source.stdin.getLines.toArray.apply(0)
+    val prot = (0 until rna.length by 3).foldLeft(new StringBuilder) {
+      case (b,i) if translator(rna.substring(i,i+3)) == "Stop" => b
+      case (b,i) => b append translator(rna.substring(i,i+3))
+    }
+    println(prot)
+  }
+}
