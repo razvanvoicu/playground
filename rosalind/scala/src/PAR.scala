375e6182f9615d01d167744c89ce63c3b1ffa61f
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-08-03 22:52:54 +0800

    Rosalind - Partition - Scala.

---
+++ rosalind/scala/src/PAR.scala
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+import scala.io.Source
+
+object PAR {
+  def main(args: Array[String]): Unit = {
+    val scanner = new Scanner(Source.stdin.bufferedReader())
+    val n = scanner.nextInt
+    val a = (1 to n).map(_ => scanner.nextInt).toArray
+    println(a.sorted.mkString(" "))
+  }
+}
