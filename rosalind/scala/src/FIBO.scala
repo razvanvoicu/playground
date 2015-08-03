aedc05a9d11be6c808daeaa7678a22871d859de1
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-08-03 22:50:58 +0800

    Rosalind - Fibonacci - Scala.

---
+++ rosalind/scala/src/FIBO.scala
@@ -0,0 +1,9 @@
+import scala.io.Source
+
+object FIBO {
+  def main(args: Array[String]): Unit = {
+    lazy val fibo : Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibo.zip(fibo.tail).map{case (x,y) => x+y}
+    val n = Source.stdin.getLines.toArray.apply(0).toInt
+    println(fibo(n))
+  }
+}
