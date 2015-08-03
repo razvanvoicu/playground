5b568821d3a3563c02f87ed4b2fa3339e84db462
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-08-03 22:56:23 +0800

    Rosalind - Binary Search - Scala.

---
+++ rosalind/scala/src/BINS.scala
@@ -0,0 +1,13 @@
+import java.util.Scanner
+import scala.io.Source
+
+object BINS {
+  def main(args: Array[String]) : Unit = {
+    val scanner = new Scanner(Source.stdin.bufferedReader())
+    val n = scanner.nextInt
+    val m = scanner.nextInt
+    val a = (1 to n).map(_ => scanner.nextInt).toArray
+    val b = (1 to m).map(_ => scanner.nextInt).toArray
+    println(b.map(x => a.indexOf(x) match {case -1 => -1 ; case k => k+1}).mkString(" "))
+  }
+}
