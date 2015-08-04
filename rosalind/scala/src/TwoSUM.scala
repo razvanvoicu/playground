1415baca24d9582042b35a130a712e3fe0aa7a79
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-08-04 13:05:24 +0800

    Rosalind - Sum of 2 equals 0 - Scala.

---
+++ rosalind/scala/src/TwoSUM.scala
@@ -0,0 +1,28 @@
+import java.util.Scanner
+import scala.io.Source
+
+object TwoSUM {
+  def main(args:Array[String]) : Unit = {
+    val inp = new Scanner(Source.stdin.bufferedReader)
+    val k = inp.nextInt
+    val n = inp.nextInt
+    (1 to k).foreach {
+      i =>
+        val a = (1 to n).map(_ => inp.nextInt)
+        val s = a.zipWithIndex.filter(_._1 >= 0)
+        val f = s.foldLeft(List[(Int, Int)]()) {
+          case (l, p) => a.indexWhere(_ == - (p._1)) match {
+            case -1 => l
+            case v if p._2 < v => (p._2+1,v+1) :: l
+            case v if p._2 > v => (v+1,p._2+1) :: l
+            case _ => l
+          }
+        }
+        val result = f match {
+          case Nil => "-1"
+          case _ => f.last._1 + " " + f.last._2
+        }
+        println(result)
+    }
+  }
+}
