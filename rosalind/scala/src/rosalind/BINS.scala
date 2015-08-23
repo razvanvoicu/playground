7cc160d1c2c2b24e9feefae17fd2aa2e28277c3e
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-08-23 16:34:03 +0800

    Rosalind - Binary Search converted to GUI - Scala.

--- rosalind/scala/src/rosalind/BINS.scala
+++ rosalind/scala/src/rosalind/BINS.scala
@@ -2,15 +2,22 @@ package rosalind
 
 import java.util.Scanner
 
+import ui.SolutionStringToTextArea
+
 import scala.io.Source
 
-object BINS {
+object BINS extends SolutionStringToTextArea {
   def main(args: Array[String]) : Unit = {
-    val scanner = new Scanner(Source.stdin.bufferedReader())
+    val inp = Source.stdin.getLines().mkString("\n")
+    println(solution(inp))
+  }
+
+  def solution(input:String) : String = {
+    val scanner = new Scanner(input)
     val n = scanner.nextInt
     val m = scanner.nextInt
     val a = (1 to n).map(_ => scanner.nextInt).toArray
     val b = (1 to m).map(_ => scanner.nextInt).toArray
-    println(b.map(x => a.indexOf(x) match {case -1 => -1 ; case k => k+1}).mkString(" "))
+    b.map(x => a.indexOf(x) match {case -1 => -1 ; case k => k+1}).mkString(" ")
   }
 }
